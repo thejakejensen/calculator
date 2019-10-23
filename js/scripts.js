0caad36b7413c2b4aaf57a471f446cd67b18593d
Student <example@example.com>
2019-10-23 14:27:39 -0700

    add script to remaining calc functions and add html code to match

--- js/scripts.js
+++ js/scripts.js
@@ -22,4 +22,28 @@ $(document).ready(function() {
     var result = add(number1, number2);
     $("#output").text(result);
   });
+
+  $("form#sub").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#sub1").val());
+    var number2 = parseInt($("#sub2").val());
+    var result = sub(number1, number2);
+    $("#output").text(result);
+  });
+
+  $("form#mult").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#mult1").val());
+    var number2 = parseInt($("#mult2").val());
+    var result = mult(number1, number2);
+    $("#output").text(result);
+  });
+
+  $("form#div").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#div1").val());
+    var number2 = parseInt($("#div2").val());
+    var result = div(number1, number2);
+    $("#output").text(result);
+  });
 });
