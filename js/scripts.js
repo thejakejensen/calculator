b61978cbf9576854445ce50a9405cde95453b15a
Student <example@example.com>
2019-10-24 13:58:06 -0700

    add radio buttons and condense code

--- js/scripts.js
+++ js/scripts.js
@@ -2,48 +2,34 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var sub = function(number1, number2) {
+var subtract = function(number1, number2) {
   return number1 - number2;
 };
 
-var mult = function(number1, number2) {
+var multiply = function(number1, number2) {
   return number1 * number2;
 };
 
-var div = function(number1, number2) {
+var divide = function(number1, number2) {
   return number1 / number2;
 };
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("form#calculator").submit(function() {
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var result = add(number1, number2);
-    $("#output").text(result);
-  });
-
-  $("form#sub").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#sub1").val());
-    var number2 = parseInt($("#sub2").val());
-    var result = sub(number1, number2);
-    $("#output").text(result);
-  });
-
-  $("form#mult").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#mult1").val());
-    var number2 = parseInt($("#mult2").val());
-    var result = mult(number1, number2);
-    $("#output").text(result);
-  });
-
-  $("form#div").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#div1").val());
-    var number2 = parseInt($("#div2").val());
-    var result = div(number1, number2);
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
     $("#output").text(result);
   });
 });
